9ed84c62cd05b4d62c36f60cf2c1e4043f493424
Yoon Hansol <yoonhs3434@naver.com>
2019-12-11 20:02:12 +0900

    191211

---
+++ stragedy/MuteQuack.java
@@ -0,0 +1,7 @@
+package stragedy;
+
+public class MuteQuack implements QuackBehavior {
+	public void quack() {
+		System.out.println("Á¶¿ë~");
+	}
+}
---
+++ stragedy/Squeak.java
@@ -0,0 +1,7 @@
+package stragedy;
+
+public class Squeak implements QuackBehavior {
+	public void quack() {
+		System.out.println("»à");
+	}
+}
