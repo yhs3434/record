16a9ac19d7de14572a2a63afae18ece849fd67f1
Yoon Hansol <yoonhs3434@naver.com>
2019-12-19 20:12:46 +0900

    Update command pattern

---
+++ DesignPattern/command/MacroCommand.java
@@ -0,0 +1,26 @@
+package command;
+
+public class MacroCommand implements Command {
+	Command[] commands;
+	
+	public MacroCommand(Command[] commands) {
+		this.commands = commands;
+	}
+	
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+		for (int i=0; i<commands.length; i++) {
+			commands[i].execute();
+		}
+	}
+
+	@Override
+	public void undo() {
+		// TODO Auto-generated method stub
+		for (int i=0; i<commands.length; i++) {
+			commands[i].undo();
+		}
+	}
+
+}
---
+++ DesignPattern/command/RemoteControl.java
@@ -0,0 +1,37 @@
+package command;
+
+public class RemoteControl {
+	Command [] onCommands;
+	Command [] offCommands;
+	Command undoCommand;
+	
+	public RemoteControl() {
+		onCommands = new Command[7];
+		offCommands = new Command[7];
+		
+		Command noCommand = new NoCommand();
+		for (int i=0; i<7; i++) {
+			onCommands[i] = noCommand;
+			offCommands[i] = noCommand;
+		}
+	}
+	
+	public void setCommand(int slot, Command onCommand, Command offCommand) {
+		this.onCommands[slot] = onCommand;
+		this.offCommands[slot] = offCommand;
+	}
+	
+	public void onButtonWasPushed(int slot) {
+		onCommands[slot].execute();
+		undoCommand = onCommands[slot];
+	}
+	
+	public void offButtonWasPushed(int slot) {
+		offCommands[slot].execute();
+		undoCommand = offCommands[slot];
+	}
+	
+	public void undo() {
+		undoCommand.undo();
+	}
+}
