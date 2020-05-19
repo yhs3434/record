8e538e08c899328ab6b1904fd7f8b07fe09ac7de
Yoon Han Sol <yoonhs3434@naver.com>
2020-05-19 20:48:38 +0900

    코어자바스크립트 끝

---
+++ CoreJavascript/200517/0.js
@@ -0,0 +1,11 @@
+// 프로토타입
+
+var Constructor = function (name) {
+  this.name = name;
+};
+Constructor.prototype.method1 = function() {};
+Constructor.prototype.property1 = 'Constructor Prototype Property';
+
+var instance = new Constructor('Instance');
+console.dir(Constructor);
+console.dir(instance);
