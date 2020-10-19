86633dde6f68e1c05b25c1b88788ad68bb98c5cc
Yoon Han Sol <yoonhs3434@naver.com>
2020-10-19 22:16:28 +0900

    ~ css background

---
+++ Css/201019/README.md
@@ -0,0 +1,8 @@
+# 201019
+
+w3 schools가 진짜 잘 만들었네..
+
+실습 하기에도 좋고, 연습문제도 중간중간 풀 수 있게 해놨구나 ㄷㄷ.
+
+
+~ https://www.w3schools.com/css/css_background_shorthand.asp
--- WooTechCamp/VanillaJs/src/DomFactory.js
+++
@@ -1,5 +0,0 @@
-export function _createElement(target, tagName) {
-    const newElem = document.createElement(tagName);
-    target.appendChild(newElem);
-    return newElem;
-}
