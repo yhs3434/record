28f8531e972657dd6f564315e1388bc9abc80e93
Yoon Han Sol <yoonhs3434@naver.com>
2020-10-19 15:22:51 +0900

    201019

--- Electron/my-electron-app/main.js
+++ Electron/my-electron-app/main.js
@@ -26,3 +26,11 @@ app.on('activate', () => {
         createWindow();
     }
 });
+
+const myNotification = new Notification('Title', {
+    body: 'Lorem Ipsum Dolor Sit Amet'
+});
+
+myNotification.onclick = () => {
+    console.log('Notification clicked');
+}
