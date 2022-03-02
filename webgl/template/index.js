ad45bb99e03df3003c76eec947594ce7aaf2d5bc
Hansol Yoon <yhs3434@gmail.com>
2022-03-03 00:39:20 +0900

    Adding 2d content to a WebGL context...

--- webgl/template/index.js
+++ webgl/template/index.js
@@ -25,3 +25,63 @@ function initWebGL(canvas) {
 
     return gl;
 }
+
+function initShader() {
+    var fragmentShader = getShader(gl, "shader-fs");
+    var vertexShader = getShader(gl, "shader-vs");
+
+    console.log('d fragmentShader', fragmentShader);
+    console.log('d vertexShader', vertexShader);
+
+    shaderProgram = gl.createProgram();
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
+    gl.linkProgram(shaderProgram);
+
+    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+
+    }
+
+    gl.useProgram(shaderProgram);
+
+    vertextPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
+    gl.enableVertexAttribArray(vertextPositionAttribute);
+}
+
+function getShader(gl, id) {
+    var shaderScript, theSource, currentChild, shader;
+
+    shaderScript = document.getElementById(id);
+
+    if (!shaderScript) {
+        return null;
+    }
+
+    theSource = '';
+    currentChild = shaderScript.firstChild;
+
+    while (currentChild) {
+        if (currentChild.nodeType == currentChild.TEXT_NODE) {
+            theSource += currentChild.textContent;
+        }
+
+        currentChild = currentChild.nextSibling;
+    }
+
+    if (shaderScript.type == 'x-shader/x-fragment') {
+        shader = gl.createShader(gl.FRAGMENT_SHADER);
+    } else if (shaderScript.type == 'x-shader/x-vertex') {
+        shader = gl.createShader(gl.VERTEX_SHADER);
+    } else {
+        return null;
+    }
+
+    gl.shaderSource(shader, theSource);
+    gl.compileShader(shader);
+
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        return null;
+    }
+
+    return shader;
+}
