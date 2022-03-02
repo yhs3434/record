902613b148d96bc5a27f1e99872297cf785255a5
Hansol Yoon <yhs3434@gmail.com>
2022-03-02 23:57:36 +0900

    getting started with WebGL.

---
+++ webgl/220302/readme.md
@@ -0,0 +1,5 @@
+link:
+
+Getting started with WebGL (https://developer.mozilla.org/ko/docs/Web/API/WebGL_API/Tutorial/Getting_started_with_WebGL)
+
+~
---
+++ webgl/template/index.js
@@ -0,0 +1,27 @@
+var gl;
+
+function start() {
+    var canvas = document.getElementById('glcanvas');
+    gl = initWebGL(canvas);
+
+    if (gl) {
+        gl.clearColor(0.0, 0.0, 0.0, 1.0);
+        gl.enable(gl.DEPTH_TEST);
+        gl.depthFunc(gl.LEQUAL);
+        gl.clear(gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT);
+    }
+}
+
+function initWebGL(canvas) {
+    gl = null;
+
+    try {
+        gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
+    } catch (e) {}
+
+    if (!gl) {
+        gl = null;
+    }
+
+    return gl;
+}
