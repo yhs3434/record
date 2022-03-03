fa72a58fcd89d2c2be2b5d455b52635613fb53c6
Hansol Yoon <yhs3434@gmail.com>
2022-03-04 00:40:11 +0900

    ~ Adding 2D content to a WebGL context

---
+++ webgl/220304/readme.md
@@ -0,0 +1,15 @@
+## link
+
+Adding 2D content to a WebGL context (https://developer.mozilla.org/ko/docs/Web/API/WebGL_API/Tutorial/Adding_2D_content_to_a_WebGL_context#%EA%B0%9D%EC%B2%B4_%EC%83%9D%EC%84%B1)
+
+~
+
+## 드는 생각
+
+1. 아 어렵다. 그래픽 처리 관련 서적을 보아야 할 것 같다.
+2. 내가 하고싶은 것을 찾은 것 같다.
+
+## 행렬 유틸리티를 이용한 연산
+
+벡터와 행렬 연산을 자바스크립트에서 다루는 데 아주 편리한 라이브러리. Sylvester.
+
--- webgl/template/index.js
+++ webgl/template/index.js
@@ -44,8 +44,8 @@ function initShader() {
 
     gl.useProgram(shaderProgram);
 
-    vertextPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
-    gl.enableVertexAttribArray(vertextPositionAttribute);
+    vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
+    gl.enableVertexAttribArray(vertexPositionAttribute);
 }
 
 function getShader(gl, id) {
@@ -84,4 +84,54 @@ function getShader(gl, id) {
     }
 
     return shader;
+}
+
+var horizAspect = 480.0/640.0;
+
+function initBuffers() {
+    squareVerticesBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, squareVerticesBuffer);
+
+    var vertices = [
+        1.0, 1.0, 0.0,
+        -1.0, 1.0, 0.0,
+        1.0, -1.0, 0.0,
+        -1.0, -1.0, 0.0
+    ];
+
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+}
+
+function drawScene() {
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    perspectiveMatrix = makePerspective(45, 640.0/480.0, 0.1, 100.0);
+
+    loadIdentity();
+    mvTranslate([-0.0, 0.0, -6.0]);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, squareVerticesBuffer);
+    gl.vertexAttribPointer(vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0);
+    setMatrixUniforms();
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+}
+
+function loadIdentity() {
+    mvMatrix = Matrix.I(4);
+}
+
+function multMatrix(m) {
+    mvMatrix = mvMatrix.x(m);
+}
+
+function mvTranslate(v) {
+    multMatrix(Matrix.Translation($V([v[0], v[1], v[2]])).ensure4x4());
+}
+
+function setMatrixUniforms() {
+    var pUniform = gl.getUniformLocation(shaderProgram, 'uPMatrix');
+    gl.uniformMatrix4fv(pUniform, false, new Float32Array(perspectiveMatrix.flatten()));
+
+    var mvUniform = gl.getUniformLocation(shaderProgram, 'uMVMatrix');
+    gl.uniformMatrix4fv(mvUniform, false, new Float32Array(mvMatrix.flatten()));
 }
